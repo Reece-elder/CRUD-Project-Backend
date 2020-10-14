e39020b32f9acf84606eef12cec902beddb427d8
reece-elder <RElder@academytrainee.com>
2020-10-14 11:05:08 +0100

    Added EqualsVerifier.simple to improve test coverage

--- src/test/java/com/main/CrudBackendApplicationTests.java
+++ src/test/java/com/main/CrudBackendApplicationTests.java
@@ -3,11 +3,18 @@ package com.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 @SpringBootTest
 class CrudBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void giveCoveragePlz() {
+		EqualsVerifier.simple();
 	}
 
 }
