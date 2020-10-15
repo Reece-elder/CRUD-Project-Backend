7139fa65316078a1812b7a8fab46976e8d7707d1
reece-elder <RElder@academytrainee.com>
2020-10-15 09:29:00 +0100

    .

--- src/main/java/com/main/persistence/domain/LogRepo.java
+++ src/main/java/com/main/persistence/domain/LogRepo.java
@@ -1,7 +1,11 @@
 package com.main.persistence.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LogRepo extends JpaRepository<Log, Long> {
+
+	List<Log> findByOrderByShipNameAsc();
 
 }
